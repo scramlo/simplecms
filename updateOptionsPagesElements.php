51f690473ff70ad88d312485fce04bd9fd3050e8
Brian Scramlin <scramlo@hotmail.com>
2016-12-02 16:10:30 -0500

    in the middle of working on elements displaying in workspace

--- updateOptionsPagesElements.php
+++ updateOptionsPagesElements.php
@@ -4,7 +4,11 @@
 
   //Set variables for easy reading
   $pageId = $_POST["pageId"];
-  $elementClass = $_POST["addElement"];
+  $elementString = $_POST["addElement"];
+  $elementArr = explode('|', $elementString);
+  $elementType = $elementArr[0];
+  $elementClass = $elementArr[1];
+  $elementName = $elementArr[2];
 
   foreach ($json->pages as $page) {
     if ($page->id == $pageId) {
@@ -12,9 +16,9 @@
       $totalElements = intval(count($page->elements));
 
       $page->elements[$totalElements]->id = $totalElements;
-      $page->elements[$totalElements]->type = "The Type";
+      $page->elements[$totalElements]->type = $elementType;
       $page->elements[$totalElements]->class = $elementClass;
-      $page->elements[$totalElements]->name = "The Name";
+      $page->elements[$totalElements]->name = $elementName;
     }
   }
 
